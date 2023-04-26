0b99ad2b3eaa10e8ad6238139d68100321137ea5
TarasKarabas <taraskyparenko@gmail.com>
2023-04-27 09:30:59 +1030

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Taras Kyparenko on 27/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
